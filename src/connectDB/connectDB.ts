345a4285e7dab6be49c8d1e6649e19ce020fd4cd
CLOTILDE FAUCHILLE <clotildefauchille@macbook-pro-de-clotilde.home>
2023-09-28 15:32:35 +0200

    pool

--- src/connectDB/connectDB.ts
+++ src/connectDB/connectDB.ts
@@ -1,24 +1,39 @@
-import { DataSource } from "typeorm";
-import dotenv from "dotenv";
-import { Client } from '../entity/Client';
-import {Adress} from '../entity/Adress';
-import {Order} from '../entity/Order';
-import {Product} from '../entity/Product';
-import {Payment} from '../entity/Payment';
+// import { DataSource } from "typeorm";
+// import dotenv from "dotenv";
+// import { Client } from '../entity/Client';
+// import {Adress} from '../entity/Adress';
+// import {Order} from '../entity/Order';
+// import {Product} from '../entity/Product';
+// import {Payment} from '../entity/Payment';
 
+// dotenv.config();
+
+// const connectDB = new DataSource({
+//   type: "postgres",
+//   host: process.env.DB_HOSTNAME,
+//   port: Number(process.env.DB_PORTNAME),
+//   username: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   entities: [Client, Adress, Order, Product, Payment],
+//   synchronize: true,
+// });
+
+
+
+// export default connectDB;
+
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const connectDB = new DataSource({
-  type: "postgres",
+const pool = new Pool({
   host: process.env.DB_HOSTNAME,
   port: Number(process.env.DB_PORTNAME),
-  username: process.env.DB_USERNAME,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [Client, Adress, Order, Product, Payment],
-  synchronize: true,
 });
 
+export default pool;
 
-
-export default connectDB;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,33 @@
-import connectDB from "./connectDB/connectDB";
+// import connectDB from "./connectDB/connectDB";
 import express from "express";
 import apiRouter from './router/api';
+import pool from './connectDB/connectDB';
 
 const app = express();
 
 const port = 4000;
 
-try {
-  const connect = async () => {
-    await connectDB.initialize();
-  };
-  connect();
-  console.log(`Data Source has been initialized`);
-} catch (error) {
-  console.error(`Data Source initialization error`, error);
-}
+// try {
+//   const connect = async () => {
+//     await connectDB.initialize();
+//   };
+//   connect();
+//   console.log(`Data Source has been initialized`);
+// } catch (error) {
+//   console.error(`Data Source initialization error`, error);
+// }
+
+app.get('/test', async (req, res)=>{
+  try {
+    const result = await pool.query('SELECT * FROM exemple');
+    console.log(result);
+    res.send(result.rows);
+
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+})
 
 app.use("/api", apiRouter);
 
